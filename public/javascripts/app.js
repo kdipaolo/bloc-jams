84f362c5163401c14f8fcda2a6f248f80dbecdd0
kdipaoloe <kurtdipaolo@gmail.com>
2015-01-15 18:05:26 -0500

    Converted landing page to angular

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -264,10 +264,32 @@ if (document.URL.match(/\/album.html/)) {
 });
 
 ;require.register("scripts/app", function(exports, require, module) {
-require("./landing"); 
-require('./collection');
-require('./album');
-require("./profile");
+ //require('./landing');
+ //require('./album');
+ //require('./collection');
+ //require('./profile');
+ 
+ angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+   $scope.subText = "Turn the music up!";
+   
+   $scope.subTextClicked = function() {
+     $scope.subText += '!';
+   };
+ 
+ 
+
+  $scope.albumURLs = [
+     '/images/album-placeholders/album-1.jpg',
+     '/images/album-placeholders/album-2.jpg',
+     '/images/album-placeholders/album-3.jpg',
+     '/images/album-placeholders/album-4.jpg',
+     '/images/album-placeholders/album-5.jpg',
+     '/images/album-placeholders/album-6.jpg',
+     '/images/album-placeholders/album-7.jpg',
+     '/images/album-placeholders/album-8.jpg',
+     '/images/album-placeholders/album-9.jpg',
+   ];
+}]);
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
@@ -366,7 +388,7 @@ var albumPicasso={name:"The Colors",artist:"Pablo Picasso",label:"Cubism",year:"
 });
 
 ;require.register("scripts/min/app-min", function(exports, require, module) {
-require("./landing"),require("./collection"),require("./album"),require("./profile");
+angular.module("BlocJams",[]).controller("Landing.controller",["$scope",function(l){l.subText="Turn the music up!",l.subTextClicked=function(){l.subText+="!"},l.albumURLs=["/images/album-placeholders/album-1.jpg","/images/album-placeholders/album-2.jpg","/images/album-placeholders/album-3.jpg","/images/album-placeholders/album-4.jpg","/images/album-placeholders/album-5.jpg","/images/album-placeholders/album-6.jpg","/images/album-placeholders/album-7.jpg","/images/album-placeholders/album-8.jpg","/images/album-placeholders/album-9.jpg"]}]);
 });
 
 ;require.register("scripts/min/profile-min", function(exports, require, module) {
