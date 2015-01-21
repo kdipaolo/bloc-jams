adab2a917fe3f7913619ed351098b7f0feea7ce2
kdipaoloe <kurtdipaolo@gmail.com>
2015-01-21 13:28:39 -0500

    Converted Bloc Jams to a single page application

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,17 +2,35 @@
  //require('./album');
  //require('./collection');
  //require('./profile');
- 
- angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
-   $scope.subText = "Turn the music up!";
-   
-   $scope.subTextClicked = function() {
-     $scope.subText += '!';
-   };
- 
- 
 
-  $scope.albumURLs = [
+
+var blocJams = angular.module('BlocJams', ['ui.router']);
+  
+
+ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+   $locationProvider.html5Mode(true);
+ 
+   $stateProvider.state('landing', {
+     url: '/',
+     controller: 'Landing.controller',
+     templateUrl: '/templates/landing.html'
+   });
+
+   $stateProvider.state('song', {
+     url: '/song',
+     templateUrl: '/templates/song.html'
+   });
+ }]);
+
+
+blocJams.controller('Landing.controller', ['$scope', function($scope) {  
+  $scope.subText = "Turn the music up!";
+
+  $scope.subTextClicked = function() {
+    $scope.subText += '!';
+  };
+
+   $scope.albumURLs = [
      '/images/album-placeholders/album-1.jpg',
      '/images/album-placeholders/album-2.jpg',
      '/images/album-placeholders/album-3.jpg',
@@ -24,3 +42,5 @@
      '/images/album-placeholders/album-9.jpg',
    ];
 }]);
+
+
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -268,17 +268,35 @@ if (document.URL.match(/\/album.html/)) {
  //require('./album');
  //require('./collection');
  //require('./profile');
- 
- angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
-   $scope.subText = "Turn the music up!";
-   
-   $scope.subTextClicked = function() {
-     $scope.subText += '!';
-   };
- 
- 
 
-  $scope.albumURLs = [
+
+var blocJams = angular.module('BlocJams', ['ui.router']);
+  
+
+ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+   $locationProvider.html5Mode(true);
+ 
+   $stateProvider.state('landing', {
+     url: '/',
+     controller: 'Landing.controller',
+     templateUrl: '/templates/landing.html'
+   });
+
+   $stateProvider.state('song', {
+     url: '/song',
+     templateUrl: '/templates/song.html'
+   });
+ }]);
+
+
+blocJams.controller('Landing.controller', ['$scope', function($scope) {  
+  $scope.subText = "Turn the music up!";
+
+  $scope.subTextClicked = function() {
+    $scope.subText += '!';
+  };
+
+   $scope.albumURLs = [
      '/images/album-placeholders/album-1.jpg',
      '/images/album-placeholders/album-2.jpg',
      '/images/album-placeholders/album-3.jpg',
@@ -290,6 +308,9 @@ if (document.URL.match(/\/album.html/)) {
      '/images/album-placeholders/album-9.jpg',
    ];
 }]);
+
+
+
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
@@ -388,7 +409,7 @@ var albumPicasso={name:"The Colors",artist:"Pablo Picasso",label:"Cubism",year:"
 });
 
 ;require.register("scripts/min/app-min", function(exports, require, module) {
-angular.module("BlocJams",[]).controller("Landing.controller",["$scope",function(l){l.subText="Turn the music up!",l.subTextClicked=function(){l.subText+="!"},l.albumURLs=["/images/album-placeholders/album-1.jpg","/images/album-placeholders/album-2.jpg","/images/album-placeholders/album-3.jpg","/images/album-placeholders/album-4.jpg","/images/album-placeholders/album-5.jpg","/images/album-placeholders/album-6.jpg","/images/album-placeholders/album-7.jpg","/images/album-placeholders/album-8.jpg","/images/album-placeholders/album-9.jpg"]}]);
+blocJams.config(["$stateProvider","$locationProvider",function(l,a){a.html5Mode(!0),l.state("landing",{url:"/",controller:"Landing.controller",templateUrl:"/templates/landing.html"})}]),blocJams.controller("Landing.controller",["$scope",function(l){l.subText="Turn the music up!",l.subTextClicked=function(){l.subText+="!"},l.albumURLs=["/images/album-placeholders/album-1.jpg","/images/album-placeholders/album-2.jpg","/images/album-placeholders/album-3.jpg","/images/album-placeholders/album-4.jpg","/images/album-placeholders/album-5.jpg","/images/album-placeholders/album-6.jpg","/images/album-placeholders/album-7.jpg","/images/album-placeholders/album-8.jpg","/images/album-placeholders/album-9.jpg"]}]);
 });
 
 ;require.register("scripts/min/profile-min", function(exports, require, module) {
