f81c330925c2b611b20db6316dbacc3f2236269b
Ricardo Vazquez <ricardo@mozillafoundation.org>
2015-01-29 10:59:01 -0500

    Fizes on spacing and missing semicolons

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,31 +1,20 @@
- //require('./landing');
- //require('./album');
- //require('./collection');
- //require('./profile');
+var albumPicasso = {
+  name: 'The Colors',
+  artist: 'Pablo Picasso',
+  label: 'Cubism',
+  year: '1881',
+  albumArtUrl: '/images/album-placeholder.png',
 
- // Example album.
- var albumPicasso = {
-   name: 'The Colors',
-   artist: 'Pablo Picasso',
-   label: 'Cubism',
-   year: '1881',
-   albumArtUrl: '/images/album-placeholder.png',
-
-   songs: [
-   { name: 'Blue', length: '4:26', audioUrl: '/music/placeholders/blue' },
-   { name: 'Green', length: '3:14', audioUrl: '/music/placeholders/green' },
-   { name: 'Red', length: '5:01', audioUrl: '/music/placeholders/red' },
-   { name: 'Pink', length: '3:21', audioUrl: '/music/placeholders/pink' },
-   { name: 'Magenta', length: '2:15', audioUrl: '/music/placeholders/magenta' }
-   ]
+  songs: [
+    { name: 'Blue', length: '4:26', audioUrl: '/music/placeholders/blue' },
+    { name: 'Green', length: '3:14', audioUrl: '/music/placeholders/green' },
+    { name: 'Red', length: '5:01', audioUrl: '/music/placeholders/red' },
+    { name: 'Pink', length: '3:21', audioUrl: '/music/placeholders/pink' },
+    { name: 'Magenta', length: '2:15', audioUrl: '/music/placeholders/magenta' }
+  ]
  };
- 
- 
-
 
  var blocJams = angular.module('BlocJams', ['ui.router']);
-
-
 
  blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
    $locationProvider.html5Mode(true);
@@ -56,7 +45,7 @@
  }]);
 
 
- blocJams.controller('Landing.controller', ['$scope', function($scope) {  
+ blocJams.controller('Landing.controller', ['$scope', function($scope) {
   $scope.subText = "Turn the music up!";
 
   $scope.subTextClicked = function() {
@@ -64,16 +53,16 @@
   };
 
   $scope.albumURLs = [
-  '/images/album-placeholders/album-1.jpg',
-  '/images/album-placeholders/album-2.jpg',
-  '/images/album-placeholders/album-3.jpg',
-  '/images/album-placeholders/album-4.jpg',
-  '/images/album-placeholders/album-5.jpg',
-  '/images/album-placeholders/album-6.jpg',
-  '/images/album-placeholders/album-7.jpg',
-  '/images/album-placeholders/album-8.jpg',
-  '/images/album-placeholders/album-9.jpg',
-  ];
+    '/images/album-placeholders/album-1.jpg',
+    '/images/album-placeholders/album-2.jpg',
+    '/images/album-placeholders/album-3.jpg',
+    '/images/album-placeholders/album-4.jpg',
+    '/images/album-placeholders/album-5.jpg',
+    '/images/album-placeholders/album-6.jpg',
+    '/images/album-placeholders/album-7.jpg',
+    '/images/album-placeholders/album-8.jpg',
+    '/images/album-placeholders/album-9.jpg'
+  ]
 }]);
 
 
@@ -87,7 +76,7 @@
 
    for (var i = 0; i < 33; i++) {
      $scope.albums.push(angular.copy(albumPicasso));
-   } 
+   }
 
    $scope.playAlbum = function(album){
      SongPlayer.setSong(album, album.songs[0]); // Targets first song in the array.
@@ -134,7 +123,7 @@ $scope.pauseSong = function(song) {
  blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
    $scope.songPlayer = SongPlayer;
  }]);
- 
+
  blocJams.service('SongPlayer', function() {
 
    var currentSoundFile = null;
@@ -206,59 +195,63 @@ $scope.pauseSong = function(song) {
 
  blocJams.directive('slider', ['$document', function($document){
 
-   // Returns a number between 0 and 1 to determine where the mouse event happened along the slider bar.
-   var calculateSliderPercentFromMouseEvent = function($slider, event) {
-     var offsetX =  event.pageX - $slider.offset().left; // Distance from left
-     var sliderWidth = $slider.width(); // Width of slider
-     var offsetXPercent = (offsetX  / sliderWidth);
-     offsetXPercent = Math.max(0, offsetXPercent);
-     offsetXPercent = Math.min(1, offsetXPercent);
-     return offsetXPercent;
-   }
+ // Returns a number between 0 and 1 to determine where the mouse event happened along the slider bar.
+ var calculateSliderPercentFromMouseEvent = function($slider, event) {
+   var offsetX =  event.pageX - $slider.offset().left; // Distance from left
+   var sliderWidth = $slider.width(); // Width of slider
+   var offsetXPercent = (offsetX  / sliderWidth);
+   offsetXPercent = Math.max(0, offsetXPercent);
+   offsetXPercent = Math.min(1, offsetXPercent);
+   return offsetXPercent;
+ }
 
-   return {
-    templateUrl: '/templates/directives/slider.html',
-    replace: true,
-    restrict: 'E',
-    scope: { }, // Creates a scope that exists only in this directive.
-    link: function(scope, element, attributes) {
-       // These values represent the progress into the song/volume bar, and its max value.
-       // For now, we're supplying arbitrary initial and max values.
-       scope.value = 0;
-       scope.max = 200;
-       var $seekBar = $(element);
+return {
+  templateUrl: '/templates/directives/slider.html',
+  replace: true,
+  restrict: 'E',
+  scope: {}, // Creates a scope that exists only in this directive.
 
-       var percentString = function () {
-         percent = Number(scope.value) / Number(scope.max)  * 100;
-         return percent + "%";
-       }
+  link: function(scope, element, attributes) {
+    // These values represent the progress into the song/volume bar, and its max value.
+    // For now, we're supplying arbitrary initial and max values.
+    scope.value = 0;
+    scope.max = 200;
+    var $seekBar = $(element);
 
-       scope.fillStyle = function() {
-         return {width: percentString()};
-       }
+    var percentString = function () {
+      percent = Number(scope.value) / Number(scope.max)  * 100;
+      return percent + "%";
+    };
 
-       scope.thumbStyle = function() {
-         return {left: percentString()};
-       }
-       scope.onClickSlider = function(event) {
-        var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
+    scope.fillStyle = function() {
+      return {width: percentString()};
+    };
+
+    scope.thumbStyle = function() {
+      return {left: percentString()};
+    };
+
+    scope.onClickSlider = function(event) {
+      var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
+      scope.value = percent * scope.max;
+      notifyCallback(scope.value);
+    };
+
+    scope.trackThumb = function() {
+      $document.bind('mousemove.thumb', function(event){
+      var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
+      scope.$apply(function(){
         scope.value = percent * scope.max;
         notifyCallback(scope.value);
-      }
+      });
+    });
 
-      scope.trackThumb = function() {
-        $document.bind('mousemove.thumb', function(event){
-          var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
-          scope.$apply(function(){
-            scope.value = percent * scope.max;
-            notifyCallback(scope.value);
-          });
-        });
+    //cleanup
+    $document.bind('mouseup.thumb', function(){
+      $document.unbind('mousemove.thumb');
+      $document.unbind('mouseup.thumb');
+    });
 
-         //cleanup
-         $document.bind('mouseup.thumb', function(){
-           $document.unbind('mousemove.thumb');
-           $document.unbind('mouseup.thumb');
-         });
-       };
-     }
+  }; //ends link function
+
+} //ends return
